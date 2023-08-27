460d2840fc8fb48ba0c80dbb2dd5f0a78e509b67
7u4bgr <abdllyevv@gmail.com>
2023-08-27 14:47:41 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+
+
+
+
+function MyFunc() { 
+    let name = document.getElementById("name").value;
+    let surname = document.getElementById("surname").value;
+    let age = document.getElementById("age").value;
+    console.log(name,surname,age);
+    document.getElementById("h1").innerHTML=" Salam "+name+" "+surname+" Sizin "+age+" yasiniz var "
+}
